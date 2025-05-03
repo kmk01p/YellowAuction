f1567c07e1c5b127b7297acc3e07baded6bd4f2d
ksh01p <114472498+ksh01p@users.noreply.github.com>
2025-05-03 15:59:58 +0900

    1
    1

--- src/main/java/com/example/yellowaution/controller/ProfileRestController.java
+++ src/main/java/com/example/yellowaution/controller/ProfileRestController.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/com/profile")
+@RequestMapping("/api/profile")
 @RequiredArgsConstructor
 public class ProfileRestController {
 
